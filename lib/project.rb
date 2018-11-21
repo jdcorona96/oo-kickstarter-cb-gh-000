8e3cd73f113f0cf9573237854177655090cb5d35
Joseph Corona <josed.hc96@gmail.com>
2018-11-21 04:50:24 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -2,8 +2,15 @@ class Project
 
   def initialize(title)
     @title = title
+    @backer = []
 
   end
 
   attr_accessor :title
+  attr_reader :backer
+
+  def add_backer(backer)
+    @backer << backer
+    
+  end
 end
